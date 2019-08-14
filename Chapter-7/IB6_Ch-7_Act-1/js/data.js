90af311e0933ecb438c1d00c2d4a00d842d24787
Tabish Tanseef <tabishtanseef@gmail.com>
2019-08-14 17:51:11 +0530

    innovation bio 6

---
+++ Chapter-7/IB6_Ch-7_Act-1/js/data.js
@@ -0,0 +1,27 @@
+var heading = "Drag the words";
+var helpTxt = "Next from the words given below each sentence drag the noun and the adjective that describes it to the correct columns.";
+var activitTitle = "Drag the words.";
+var msgComplete = "End of Activity";
+var btnLabel = {
+	"submitbtn": "submit",
+	"nextbtn" : "SHOW Answer",
+	"tryAgain": "Try Again"
+}
+
+
+var dataCollection = [
+	"<span class='draggable'>Diabetes</span> | <span class='draggable'>Rickets</span> | <span class='draggable'>Chicken pox</span> | <span class='draggable'>Tuberculosis</span>", 
+	"<span class='draggable'>Cold and cough</span> | <span class='draggable'>Anemia</span> | <span class='draggable'>Cholera</span> | <span class='draggable'>Heart attack</span>"
+]
+
+var hardTh = ["Chicken pox","Cholera","Tuberculosis","Cold and cough"];
+ 
+var tTh = ["Rickets","Heart attack","Anemia","Diabetes"];
+
+var dTh=[];
+
+var softTh = [];
+
+
+
+
